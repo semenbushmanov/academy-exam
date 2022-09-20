1be4775e0e5729ad4cff72bbaa9b6bb70dc780e0
Semen Bushmanov <bush.sem@mail.ru>
2022-09-20 20:39:40 +0500

    add api and install axios, toastify, http-status-codes

---
+++ src/services/api.ts
@@ -0,0 +1,35 @@
+import axios, { AxiosInstance, AxiosResponse, AxiosError } from 'axios';
+import { StatusCodes } from 'http-status-codes';
+import { toast } from 'react-toastify';
+
+const StatusCodeMapping: Record<number, boolean> = {
+  [StatusCodes.BAD_REQUEST]: true,
+  [StatusCodes.NOT_FOUND]: true
+};
+
+const shouldDisplayError = (response: AxiosResponse) => !!StatusCodeMapping[response.status];
+
+const BACKEND_URL = 'https://10.react.pages.academy/wtw';
+const REQUEST_TIMEOUT = 5000;
+
+const createAPI = (): AxiosInstance => {
+  const api = axios.create({
+    baseURL: BACKEND_URL,
+    timeout: REQUEST_TIMEOUT,
+  });
+
+  api.interceptors.response.use(
+    (response) => response,
+    (error: AxiosError<any>) => {
+      if (error.response && shouldDisplayError(error.response)) {
+        toast.warn(error.response.data.error);
+      }
+
+      throw error;
+    }
+  );
+
+  return api;
+};
+
+export const api = createAPI();
