f87efca06e4057152220ec2592968b3386a241cb
Semen Bushmanov <bush.sem@mail.ru>
2022-09-21 00:47:13 +0500

    add props to quest catalog, add rendering of quests received from server on home page

--- src/components/home/home.tsx
+++ src/components/home/home.tsx
@@ -6,17 +6,23 @@ import {
 } from 'components/common/common';
 import { QuestsCatalog } from './components/components';
 import * as S from './home.styled';
+import { useAppSelector } from '../../hooks/index';
+import { getQuests } from '../../store/quests-data/selectors';
 
-const HomePage = (): JSX.Element => (
-  <MainLayout>
-    <S.Main forwardedAs="main">
-      <PageHeading>
-        <PageTitle>Выберите тематику</PageTitle>
-        <PageSubtext>квесты в Санкт-Петербурге</PageSubtext>
-      </PageHeading>
-      <QuestsCatalog />
-    </S.Main>
-  </MainLayout>
-);
+const HomePage = (): JSX.Element => {
+  const quests = useAppSelector(getQuests);
+
+  return (
+    <MainLayout>
+      <S.Main forwardedAs="main">
+        <PageHeading>
+          <PageTitle>Выберите тематику</PageTitle>
+          <PageSubtext>квесты в Санкт-Петербурге</PageSubtext>
+        </PageHeading>
+        <QuestsCatalog quests={quests}/>
+      </S.Main>
+    </MainLayout>
+  );
+};
 
 export default HomePage;
